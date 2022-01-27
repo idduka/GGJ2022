a8c23a12f5fbb38ceda68a45e2d302f82952a1d2
idduka <idduka@gmail.com>
2022-01-27 11:42:18 +0100

    - Configured game inputs for keyboard and controllers.

--- Assets/Scripts/HaqAlla.cs
+++ Assets/Scripts/HaqAlla.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HaqAlla : MonoBehaviour
 {
     public float Speed = 10.0f;
+    public bool IsPlayer1 = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,10 @@ public class HaqAlla : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        var horizontalAxisName = IsPlayer1 ? "P1Horizontal" : "P2Horizontal";
+        var fireAxisName = IsPlayer1 ? "P1Fire" : "P2Fire";
+
+        var direction = new Vector3(Input.GetAxis(horizontalAxisName), 0.0f, 0.0f);
         direction.Normalize();
         transform.Translate(direction * Speed * Time.deltaTime);
     }
