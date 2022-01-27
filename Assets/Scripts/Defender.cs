668420dd1d46902bdd10b4bbbf8bc76d77e6ce6d
idduka <idduka@gmail.com>
2022-01-27 12:52:00 +0100

    - Added defender sprite and component.

---
+++ Assets/Scripts/Defender.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Defender : MonoBehaviour
+{
+    public bool IsPlayer1 = true;
+    public Planet HomePlanet;
+    public float RadialSpeed = 180;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.isEditor && !Application.isPlaying)
+        {
+            // Control and update defender properties while in Editor mode...
+            if (HomePlanet != null)
+            {
+                transform.localPosition = new Vector3(0.0f, HomePlanet.Radius, 0.0f);
+            }
+        }
+        else
+        {
+            var horizontalAxisName = IsPlayer1 ? "P1Horizontal" : "P2Horizontal";
+            var fireAxisName = IsPlayer1 ? "P1Fire" : "P2Fire";
+
+            //var direction = new Vector3(Input.GetAxis(horizontalAxisName), 0.0f, 0.0f).normalized;
+            //transform.Translate(direction * RadialSpeed * Time.deltaTime);
+        }
+    }
+}
