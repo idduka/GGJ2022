4ff3f1c1db85cf929643086890884190f99f46b1
clinf <mizziclint@gmail.com>
2022-01-28 22:36:25 +0100

    singleplayer option

--- Assets/Scripts/ButtonSinglePlayer.cs
+++ Assets/Scripts/ButtonSinglePlayer.cs
@@ -2,38 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 using UnityEngine.SceneManagement;
-
-public class ButtonNewScene : MonoBehaviour
+public class ButtonSinglePlayer : MonoBehaviour
 {
-
-
     // Start is called before the first frame update
+    public void StartSinglePlayer()
+    {
 
-    public void NextScene()
-    { 
-               
-            { 
-        
+        {
+            SceneManager.LoadScene("GameMain", LoadSceneMode.Single);
             
-           
-            SceneManager.LoadScene("GameMain",LoadSceneMode.Single);
-            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+
+                //  player = gameObjects.[]
+                // SceneManager.LoadScene("GameMain", LoadSceneMode.Single);
+                SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+
+            }
         }
-    }
+
+
+    
 
     private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
-
         Scene s = SceneManager.GetSceneByName("GameMain");
         GameObject[] gameObjects = s.GetRootGameObjects();
-        GameObject player;
+      
+       
         foreach (GameObject go in gameObjects)
         {
+        
             Debug.Log(go.name);
             if (go.name == "Player1Zone")
-
+            
             {
                 DefenderAI AIscript;
                 AIscript = go.GetComponentInChildren<DefenderAI>();
@@ -45,11 +46,12 @@ public class ButtonNewScene : MonoBehaviour
             {
                 DefenderAI AIscript;
                 AIscript = go.GetComponentInChildren<DefenderAI>();
-                AIscript.enabled = false;
-               
+                AIscript.enabled = true;
+                
             }
 
         }
+
     }
 }
 
