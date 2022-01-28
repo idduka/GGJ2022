7d9094dbf2e99270fcd7ede64df5be7bdf2b9d5d
idduka <idduka@gmail.com>
2022-01-28 11:57:32 +0100

    - Added phasing in/out for enemies.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -10,7 +11,17 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     [Tooltip("The sprite that is applied when the enemy respawns on the other player side.")]
     private Sprite _respawnSprite;
-    
+
+    [SerializeField]
+    [Tooltip("The total time it would take to phase out or phase in.")]
+    private float _totalPhaseDuration;
+
+    [SerializeField]
+    [Tooltip("The transition curve for phasing out and in.")]
+    private AnimationCurve _phaseAnimation;
+
+    private bool _isPhasing = false;
+
     private SpriteRenderer _spriteRenderer;
     
     public Planet TargetPlanet { get; set; }
@@ -20,6 +31,14 @@ public class Enemy : MonoBehaviour
     public bool IsRespawn { get; set; }
     
     public EnemySpawner EnemySpawner { private get; set; }
+
+    private void Start()
+    {
+        if(IsRespawn)
+        {
+            StartCoroutine(PhaseIn());
+        }
+    }
 
     public void ChangeSprite()
     {
@@ -33,6 +52,11 @@ public class Enemy : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if(_isPhasing)
+        {
+            return;
+        }
+
         var planet = other.GetComponent<Planet>();
 
         Debug.Log($"HIT PLANET: {planet != null}");
@@ -48,15 +72,58 @@ public class Enemy : MonoBehaviour
 
         if (projectile != null)
         {
+            Destroy(projectile.gameObject);
             if (!IsRespawn)
             {
-                EnemySpawner.SpawnEnemyAtOtherSide(transform.localPosition);
+                StartCoroutine(PhaseOut());
             }
-            
-            Debug.Log($"Killed enemy position: {transform.localPosition.x}, {transform.localPosition.y}");
-            EnemySpawner.AliveEnemies.Remove(this);
-            Destroy(projectile.gameObject);
-            Destroy(gameObject);
+            else
+            {
+                Die();
+            }
         }
+    }
+
+    public IEnumerator PhaseIn()
+    {
+        _isPhasing = true;
+        _speed = 0.0f;
+        var curPhaseDuration = 0.0f;
+        while (curPhaseDuration <= _totalPhaseDuration)
+        {
+            var colorComponentValue = _phaseAnimation.Evaluate(curPhaseDuration / _totalPhaseDuration);
+            var newSpriteColor = new Color(colorComponentValue, colorComponentValue, colorComponentValue, colorComponentValue);
+            GetComponent<SpriteRenderer>().color = newSpriteColor;
+            yield return null;
+            curPhaseDuration += Time.deltaTime;
+        }
+        _speed = 0.01f;
+        _isPhasing = false;
+    }
+
+    public IEnumerator PhaseOut()
+    {
+        _isPhasing = true;
+        _speed = 0.0f;
+        var curPhaseDuration = 0.0f;
+        while (curPhaseDuration <= _totalPhaseDuration)
+        {
+            var colorComponentValue = 1.0f - (_phaseAnimation.Evaluate(curPhaseDuration / _totalPhaseDuration));
+            Debug.Log($"CCV: {colorComponentValue}");
+            var newSpriteColor = new Color(colorComponentValue, colorComponentValue, colorComponentValue, colorComponentValue);
+            GetComponent<SpriteRenderer>().color = newSpriteColor;
+            yield return null;
+            curPhaseDuration += Time.deltaTime;
+        }
+        Debug.Log($"CCV: Switch!");
+        EnemySpawner.SpawnEnemyAtOtherSide(transform.localPosition);
+        Die();
+    }
+
+    private void Die()
+    {
+        Debug.Log($"Killed enemy position: {transform.localPosition.x}, {transform.localPosition.y}");
+        EnemySpawner.AliveEnemies.Remove(this);
+        Destroy(gameObject);
     }
 }
