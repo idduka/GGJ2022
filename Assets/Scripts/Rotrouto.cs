81d1829ac4a65a3bf429dc41bceacb4c42b37b8d
Mark A <macassar@gmail.com>
2022-01-29 22:45:24 +0100

    Shitty Difficulty Choice Menu

---
+++ Assets/Scripts/Rotrouto.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotrouto : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotationToAdd = new Vector3(0, 0, speed);
+        transform.Rotate(rotationToAdd);
+    }
+}
