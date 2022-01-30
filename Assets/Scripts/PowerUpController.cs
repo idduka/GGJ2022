650046b2650b9dc6675e8ea3a1047d94b62ee070
Niklas <niklaselze@web.de>
2022-01-30 12:07:54 +0100

    Added script to enforce screen ratio, added text to show power up cost

--- Assets/Scripts/PowerUpController.cs
+++ Assets/Scripts/PowerUpController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerUpController : MonoBehaviour
 {
@@ -29,10 +31,27 @@ public class PowerUpController : MonoBehaviour
     public int CloudCost;
 
     public GameState _gameState;
+
+    public Text TurretCostText;
+    public Text HealCostText;
+    public Text TriShostCostText;
+    public Text NukeCostText;
+    public Text EmpCostText;
+    public Text CloudCostText;
     
     private readonly Color _grey = new Color(0.3f, 0.3f, 0.3f);
     private readonly Color _originalColor = new Color(1, 1, 1);
-    
+
+    private void Start()
+    {
+        TurretCostText.text = TurretCost.ToString();
+        HealCostText.text = HealthCost.ToString();
+        TriShostCostText.text = TriShotCost.ToString();
+        NukeCostText.text = NukeCost.ToString();
+        EmpCostText.text = EmpCost.ToString();
+        CloudCostText.text = CloudCost.ToString();
+    }
+
     private void FixedUpdate()
     {
         if (_gameState.IsGameOver)
---
+++ Assets/Scripts/ResolutionEnforcer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ResolutionEnforcer : MonoBehaviour
+{
+    void Start()
+    {
+        SetRatio(2, 1);
+    }
+    void SetRatio(float w, float h)
+    {
+        if ((((float)Screen.width) / ((float)Screen.height)) > w / h)
+        {
+            Screen.SetResolution((int)(((float)Screen.height) * (w / h)), Screen.height, true);
+        }
+        else
+        {
+            Screen.SetResolution(Screen.width, (int)(((float)Screen.width) * (h / w)), true);
+        }
+    }
+}
